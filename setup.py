21b51fc34b3f85c49863adda253b3b60e1e46b0d
Maixent Chenebaux <max.chbx@gmail.com>
2020-06-29 12:02:18 +0200

    added c files and fixed setup to not depend on cython

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
-import numpy
 import setuptools
-from Cython.Build import cythonize
 from setuptools import Extension
 
 
 extensions = [
-    Extension("nearset/*", ["nearset/*.pyx"]),
+    Extension("nearset", sources=["nearset/__init__.c"]),
+    Extension("nearset/metrics", sources=["nearset/metrics.c"]),
 ]
 
 setuptools.setup(
     name="nearset",
-    version="0.0.1",
+    version="0.0.4",
     author="Maixent Chenebaux",
     author_email="max.chbx@gmail.com",
     description="Efficient ordered set of elements with custom comparator function in Python",
     long_description_content_type="text/markdown",
     url="https://github.com/kerighan/nearset",
     packages=setuptools.find_packages(),
-    include_path = [numpy.get_include()],
     include_package_data=True,
-    ext_modules=cythonize(extensions),
+    install_requires=["blist>=1.3.6", "numpy"],
+    ext_modules=extensions,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
