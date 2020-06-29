2a348a11de7b3bf2a3492999e4ff1743633fbaf9
Maixent Chenebaux <max.chbx@gmail.com>
2020-06-29 11:30:33 +0200

    fixed setup tools

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import numpy
 from Cython.Build import cythonize
 from setuptools import Extension
 
@@ -7,19 +8,16 @@ extensions = [
     Extension("nearset/*", ["nearset/*.pyx"]),
 ]
 
-# with open("README.md", "r") as f:
-#     long_description = f.read()
-
 setuptools.setup(
     name="nearset",
     version="0.0.1",
     author="Maixent Chenebaux",
     author_email="max.chbx@gmail.com",
-    description="Efficient ordered set of elements with comparator function in Python",
-    # long_description=long_description,
+    description="Efficient ordered set of elements with custom comparator function in Python",
     long_description_content_type="text/markdown",
     url="https://github.com/kerighan/nearset",
     packages=setuptools.find_packages(),
+    include_path = [numpy.get_include()],
     include_package_data=True,
     ext_modules=cythonize(extensions),
     classifiers=[
